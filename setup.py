e437c1fdd71f529f72a510db69ef547805a08a04
wblack <will.black@conversocial.com>
2018-07-03 15:01:09 +0000

    feat: noticket - why would you define VERSION in two different places? *RAGE*

--- setup.py
+++ setup.py
@@ -7,25 +7,29 @@ if not root_dir:
     root_dir = '.'
 long_desc = open(root_dir + '/README.rst').read()
 
+init = os.path.join(root_dir, 'djangosampler', '__init__.py')
+version_line = [l for l in open(init) if l.startswith('VERSION')][0]
+VERSION = eval(version_line.split('=')[-1])
+
 setup(
-	name='djangosampler',
-	version='0.10.0',
-	description='Samples a percentage of SQL queries and groups them together for easy viewing',
-	url='https://github.com/colinhowe/djangosampler',
-	author='Colin Howe',
-	author_email='colin@colinhowe.co.uk',
-	packages=['djangosampler', 'djangosampler.plugins', 'djangosampler.migrations'],
-    package_data={'djangosampler': ['templates/djangosampler/*','static/djangosampler/*']},
-	classifiers=[
-		'Development Status :: 4 - Beta',
-		'Environment :: Web Environment',
-		'Framework :: Django',
-		'Intended Audience :: Developers',
+    name='djangosampler',
+    version=VERSION,
+    description='Samples a percentage of SQL queries and groups them together for easy viewing',
+    url='https://github.com/colinhowe/djangosampler',
+    author='Colin Howe',
+    author_email='colin@colinhowe.co.uk',
+    packages=['djangosampler', 'djangosampler.plugins', 'djangosampler.migrations'],
+    package_data={'djangosampler': ['templates/djangosampler/*', 'static/djangosampler/*']},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python',
-		'Topic :: Software Development :: Libraries :: Python Modules',
-	],
-	license='Apache 2.0',
-	long_description=long_desc,
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    license='Apache 2.0',
+    long_description=long_desc,
 )
